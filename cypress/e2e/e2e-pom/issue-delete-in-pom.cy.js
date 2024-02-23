78d1cc71d5f872feda485584c8dcc1d35a5861a1
Ilja2103 <ilja.ollokainen@gmail.com>
2024-02-23 17:13:03 +0200

    Drag and drop tests

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -1,25 +1,38 @@
-/**
- * This is an example file and approach for POM in Cypress
- */
 import IssueModal from "../../pages/IssueModal";
 
-describe('Issue delete', () => {
+const issueTitle = "This is an issue of type: Task.";
+
+describe("Issue delete", () => {
   beforeEach(() => {
-    cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
-    cy.contains(issueTitle).click();
+    cy.visit("/");
+    cy.url().should("eq", `${Cypress.env("baseUrl")}project/board`).then((url) => {
+      // Open issue detail modal with title from line 16
+      cy.contains(issueTitle).click();
     });
   });
 
-  //issue title, that we are testing with, saved into variable
-  const issueTitle = 'This is an issue of type: Task.';
+  it("Should delete issue", () => {
+    // Click the delete button and confirm deletion
+    IssueModal.clickDeleteButton();
+    IssueModal.confirmDeletion();
 
-  it('Should delete issue successfully', () => {
-    //add steps to delete issue
+    // Validate that the issue is not visible
+    IssueModal.validateIssueVisibilityState(issueTitle, false);
   });
 
-  it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
+  it("Should cancel deletion process", () => {
+    // Ensure the issue detail modal is visible
+    IssueModal.getIssueDetailModal().should("be.visible");
+
+    // Click the delete button, cancel deletion, and validate state
+    IssueModal.clickDeleteButton();
+    IssueModal.cancelDeletion();
+
+    // Confirm that the confirmation dialog is not present
+    cy.get('[data-testid="modal:confirmation-dialog"]').should("not.exist");
+
+    // Close the detail modal and validate that the issue is still visible
+    IssueModal.closeDetailModal();
+    IssueModal.validateIssueVisibilityState(issueTitle, true);
   });
 });
